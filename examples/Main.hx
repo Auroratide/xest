1dbd3a4129b26aa18f752b3ae6a023b4f38c9272
auroratide <tf.auroratide@gmail.com>
2019-09-03 18:36:02 -0500

    Unify test examples with the rest of how tests are run

--- examples/Main.hx
+++ examples/Main.hx
@@ -2,26 +2,13 @@ package ;
 
 import com.auroratide.xest.Xest.start;
 
-using Lambda;
-
 class Main {
   public static function main() {
-    testTheTestRunner();
-
     start([
+      new running.RunningTests(),
       new assertions.Assertions(),
       new running.hooks.BeforeEach(),
       new running.hooks.AfterEach()
     ]);
   }
-
-  private static function testTheTestRunner() {
-    final test = new running.RunningTests();
-
-    start([ test ]);
-
-    if(!test.itRan.foreach(b -> b))
-      throw 'Expected all tests in RunningTests to run, but they did not: ${test.itRan}';
-  }
-
 }
--- examples/running/RunningTests.hx
+++ examples/running/RunningTests.hx
@@ -3,20 +3,18 @@ package running;
 import com.auroratide.xest.Xest;
 
 class RunningTests extends Xest {
-  public final itRan = [ false, false, false ];
-
   public function new() {
     example("how to create a test", () -> {
-      itRan[0] = true;
+      assert(true).is(true);
     });
 
     describe("Aliases", () -> {
       it("allows 'it' to define tests also", () -> {
-        itRan[1] = true;
+        assert(true).is(true);
       });
 
       test("allows 'test' to define tests also", () -> {
-        itRan[2] = true;
+        assert(true).is(true);
       });
     });
   }

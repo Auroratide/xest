8919ef6e601f0e4b09931840ff6b0e051d344639
auroratide <tf.auroratide@gmail.com>
2019-09-22 16:57:25 -0500

    Stubbing tests now better demonstrate scenarios

--- examples/faking/Stubbing.hx
+++ examples/faking/Stubbing.hx
@@ -10,23 +10,30 @@ class Stubbing extends Xest {
       sample = fake(SampleClass);
     });
 
-    example("stubbing a method with no arguments", () -> {
+    example("stubbing methods", () -> {
       stub(sample.noArgs()).toReturn(2);
+      stub(sample.oneArg(1)).toReturn(3);
+      stub(sample.twoArgs(2, "xest")).toReturn(5);
 
       expect(sample.noArgs() == 2);
+      expect(sample.oneArg(1) == 3);
+      expect(sample.twoArgs(2, "xest") == 5);
     });
 
-    example("stubbing a method with one argument", () -> {
+    example("null is returned when no stub is defined", () -> {      
       stub(sample.oneArg(1)).toReturn(2);
-      stub(sample.oneArg(2)).toReturn(3);
 
-      expect(sample.oneArg(2) == 3);
-      expect(sample.oneArg(1) == 2);
+      expect(sample.noArgs() == null);
+      expect(sample.oneArg(2) == null);
     });
 
-    example("stubbing a method with multiple arguments", () -> {
+    example("arguments can be used to identify which stub is returned", () -> {
       stub(sample.twoArgs(1, "xest")).toReturn(2);
+      stub(sample.twoArgs(2, "xest")).toReturn(3);
+      stub(sample.twoArgs(2, "is good")).toReturn(5);
 
+      expect(sample.twoArgs(2, "is good") == 5);
+      expect(sample.twoArgs(2, "xest") == 3);
       expect(sample.twoArgs(1, "xest") == 2);
     });
   }
